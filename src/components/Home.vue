c1121f78173144cfe57605d67611465c95e8dbf3
Madison Nelson <madison.r.nelson@gmail.com>
2020-02-10 11:13:47 -0700

    edited Home to fill full screen, still need to darken text

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,14 +1,35 @@
 <template>
-    <div :style="{backgroundImage: `url(${backgroundUrl})`}" style="margin-left: 32px;">
-        <div style="height: 550px;">
+    <section>
+    <div :style="{backgroundImage: `url(${backgroundUrl})`}" style="margin-left: 0px; background-size: cover">
+        <div style="height: 550px;" >
             <div style="background-color: white; opacity: 0.7; position: absolute;
-                margin-left: 7.5%; margin-top: 5%; height: 80%; width: 80%;">
-                <div style="color: white;">
-                    HOME
+                margin-left: 10%; margin-right: 10%; margin-top: 5%; height: 80%; width: 80%;">
+                <div style="text-align: center; padding-top: 30px; font-size: 50px; color: #628CB6; font-weight: bold; opacity: 0%">
+                    Reserve a room. Any building. Any time.
+                </div>
+                <br>
+                <br>
+                <br>
+                <div style="text-align: center;">
+                    <v-btn
+                            rounded
+                            color="#628CB6"
+                            style="color: white; display: inline-block;"
+                            :disabled="time === ''"
+                    >
+                        Get Started!
+                    </v-btn>
+                </div>
+                <br>
+                <div style="text-align: center; font-size: 20px; color: #628CB6; text-decoration: underline">
+                    Log In
                 </div>
             </div>
+
         </div>
     </div>
+
+    </section>
 </template>
 
 <script>
